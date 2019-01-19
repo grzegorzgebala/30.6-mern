e0d6bc1ee645ada613a82bbd57022d444d3a5b2f
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2019-01-19 15:36:07 +0100

    Final commit

---
+++ client/modules/About/About.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Import Style
+
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>It is About Site!</h2>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+About.propTypes = {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About);
---
+++ client/modules/App/components/Header/Header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import { FormattedMessage } from 'react-intl';
+
+// Import Style
+import styles from './Header.css';
+
+export function Header(props, context) {
+  const languageNodes = props.intl.enabledLanguages.map(
+    lang => <li key={lang} onClick={() => props.switchLanguage(lang)} className={lang === props.intl.locale ? styles.selected : ''}>{lang}</li>
+  );
+
+  return (
+    <div className={styles.header}>
+      <div className={styles['language-switcher']}>
+        <div className="pol">
+          <ul>
+            <span className="nav">
+              <li><Link to="/home">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/">Posts</Link></li>
+            </span>
+            <span>
+              <li><FormattedMessage id="switchLanguage" /></li>
+              {languageNodes}
+            </span>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles['site-title']}>
+          <Link to="/" ><FormattedMessage id="siteTitle" /></Link>
+        </h1>
+        {
+          context.router.isActive('/', true)
+            ? <a className={styles['add-post-button']} href="#" onClick={props.toggleAddPost}><FormattedMessage id="addPost" /></a>
+            : null
+        }
+      </div>
+    </div>
+  );
+}
+
+Header.contextTypes = {
+  router: PropTypes.object,
+};
+
+Header.propTypes = {
+  toggleAddPost: PropTypes.func.isRequired,
+  switchLanguage: PropTypes.func.isRequired,
+  intl: PropTypes.object.isRequired,
+};
+
+export default Header;
